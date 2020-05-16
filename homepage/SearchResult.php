8306c5223af74521fbd3536cc71a145df17030b7
ms-brahim <brahim7khalil@gmail.com>
2020-05-16 23:18:07 +0000

    improved frontend

--- homepage/SearchResult.php
+++ homepage/SearchResult.php
@@ -179,7 +179,54 @@
 		</div>
 		
 	</div>
+	<!-- end content  -->
 
+	<!-- start footer  -->
+	<footer>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm">
+					<img src="/assets/logo.png" width="100" alt="logo">
+					<h3>About us</h3>
+					<p> Dolores deleniti esse sit fuga sunt fugit numquam, unde soluta quae autem natus quam asperiores minima consequuntur repellendus similique? Eligendi, facere quod!</p>
+
+				</div>
+				<div class="col-sm">
+					<label for="comment">Comment:</label>
+					<textarea class="form-control" rows="5" id="comment"></textarea>
+					<input type="submit" class="btn btn-info" value="Send Your Message" style="margin-top: 10px;">
+				</div>
+				<div class="col-sm">
+					<div class="mu-footer-widget">
+						<h4>Business Offers</h4>
+						<ul class="list-group">
+							<a href="" class="listOffers">Business trip</a><br>
+							<a href="" class="listOffers">Beyond Business</a><br>
+							<a href="" class="listOffers">Meetings and conferences</a>
+						</ul>
+						
+						
+					  </div>
+				</div>
+				<div class="col-sm-2">
+					<h3>Follow us</h3>
+					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam, et cumque iure cum sint accusantium praesentium recusandae </p>
+					<div class="row justify-content-around">
+						<i class="fa fa-whatsapp" aria-hidden="true"></i>
+						<i class="fa fa-linkedin" aria-hidden="true"></i>
+						<i class="fa fa-twitter" aria-hidden="true"></i>
+						<i class="fa fa-facebook" aria-hidden="true"></i>
+						<i class="fa fa-instagram" aria-hidden="true"></i>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		<div class="col" style="margin-top: 40px;background-color: gainsboro;">
+			<p>Copyright-2020 ©  By : You<span style="color:blue">code</span> Safi</p>
+		</div>
+	</footer>
+	<!-- end footer  -->
 
 
 	<script src="assets/js/search.js"> </script>
